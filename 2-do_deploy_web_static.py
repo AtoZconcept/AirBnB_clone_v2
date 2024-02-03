6831f81459678eca9d56ea9dc981d9d234b30fa3
AtoZconcept <opeyemiolaafeez.com>
2024-02-03 17:01:12 +0100

    add

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -31,7 +31,11 @@ def do_deploy(archive_path):
         # Delete the archive from the web server
         run("rm /tmp/{}".format(basename))
 
+        # Move the files
+        run("mv {0}web_static/* {0}".format(path))
+
         # Delete the symbolic links from the web server
+        run("rm -rf {}web_static".format(path))
         run("rm -rf /data/web_static/current")
 
         # Create a new the symbolic link
